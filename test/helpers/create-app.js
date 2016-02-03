8ff2cd5b3e3afafdce8647ba8eea11897b163fe3
Offir Golan <offirgolan@yahoo-inc.com>
2016-02-03 14:24:02 -0800

    add no env config test

--- test/helpers/create-app.js
+++ test/helpers/create-app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var injectMeta = require('../../lib/inject-meta');
 
-module.exports = function createApp(inject) {
+module.exports = function createApp(inject, html) {
+  html = html || 'default';
+  html += '.html';
   var app = express();
   app.use(injectMeta(inject));
   app.get('/', function(req, res) {
-      res.sendFile(path.join(__dirname, '/test-index.html'));
+      res.sendFile(path.join(__dirname, 'html', html));
   });
   return app;
 };
