846ad59927776fd192c528e25172289325eb0cc5
Offir Golan <offirgolan@gmail.com>
2016-02-02 17:55:55 -0800

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-/**
- * Export inject-meta (with )
- */
-
-exports = module.exports = require('./lib/inject-meta');
-
-/*
-  Export the version
-*/
-
-exports.version = require('./package').version;
+module.exports = require('./lib/inject-meta');
